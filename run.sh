d68d81893d232162cc4563c5e4f2dcaf7aaf7ef4
Angelo Alvarez <steelcuts@hotmail.com>
2026-04-26 23:15:23 +0200

    fix: BUILD_FROM var

--- run.sh
+++ run.sh
@@ -1,14 +1,8 @@
-#!/usr/bin/env bashio
+#!/bin/bash
 
 echo "Starting IR2MQTT backend in Home Assistant Add-on mode..."
 
-# Default port
-PORT=8099
-
-if bashio::config.has 'ingress_port'; then
-    PORT=$(bashio::addon.ingress_port)
-    echo "Using Ingress port ${PORT}."
-fi
+PORT=${INGRESS_PORT:-8099}
 
 # Run pending database migrations
 python3 -m alembic upgrade head
